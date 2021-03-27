9e6144e0bc1f04fb0cab8aca98ac60334af792d4
Oguz Kinsan <oguzkinsan@hotmail.com>
2021-03-27 21:11:17 +0300

    12. gün sonu

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,5 +1,8 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluetValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validatiton;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -21,12 +24,9 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         public Result Add(Car car)
         {
-            if (car.CarName.Length<2)
-            {
-                return new ErrorResult(Messages.CarNameInvalid);
-            }
             _carDal.Add(car);
             return new SuccessResult(Messages.CarAdded);
         }
---
+++ Business/ValidationRules/FluetValidation/CarValidator.cs
@@ -0,0 +1,20 @@
+﻿using Business.Constants;
+using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluetValidation
+{
+    public class CarValidator:AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.CarName).MinimumLength(2).WithMessage(Messages.CarNameInvalid);
+            RuleFor(c => c.DailyPrice).GreaterThanOrEqualTo(100);
+        }
+    }
+}
